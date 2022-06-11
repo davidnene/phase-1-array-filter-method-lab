f2d0b89b6926b2bb408efcff3e1b05a272321c7b
davidnene <davienesh4@gmail.com>
2022-06-11 12:09:22 +0300

    commit tree 85e26f37a901d038a1a2ac4153898eae5af240d0

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution here
+//drivers = ['Bill','Atino','Fil','Pat','Kel','Ah', 'bill']
+
+function findMatching (array, name) {
+    const results = array.filter((word) => name.toUpperCase() === word.toUpperCase() );
+    return results;
+}
+
+function fuzzyMatch(array, name) {
+    const results = array.filter((word) => word.slice(0,2) === name.slice(0,2));
+    return results;
+}
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(array,name) {
+      const results = array.filter(
+          (arr) =>
+         arr.name === name)
+      return results;
+  }
+
+console.log(drivers.name)
+
+console.log(matchName(drivers,'Bobby'))
+
+let words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present']
+
+const modifiedWords = words.filter( (word, index, arr) => {
+  arr[index+1] +=' extra'
+  return word.length < 6
+})
+
+console.log(modifiedWords)
